4e6f0c98a49458cbd34dc0bf0d425f163a9f8822
Ifeh04 <100967038+Ifeh04@users.noreply.github.com>
2026-03-07 20:56:09 +0100

    Format SQL schema for better readability

--- sql/01_schema_setup.sql
+++ sql/01_schema_setup.sql
@@ -15,13 +15,14 @@ CREATE TABLE sales(
     product_line VARCHAR(100) NOT NULL,
     unit_price DECIMAL(10, 2) NOT NULL,
     quantity INT NOT NULL,
-    VAT DECIMAL(6, 4) NOT NULL,
+    VAT DECIMAL(6, 4) NOT NULL,                   
     total DECIMAL(10, 2) NOT NULL,
     date DATE NOT NULL,
     time TIME NOT NULL,
     payment_method VARCHAR(15) NOT NULL,
     cogs DECIMAL(10, 2) NOT NULL,
-    gross_margin_percentage DECIMAL(10, 9) NOT NULL,
+    gross_margin_percentage DECIMAL(10, 9) NOT NULL, 
     gross_income DECIMAL(12, 4) NOT NULL,
-    rating DECIMAL(3, 2) NOT NULL
+    rating DECIMAL(3, 2) NOT NULL                   
 );
+
